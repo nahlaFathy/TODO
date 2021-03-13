b6045da8f042bc3b8fdabf36ca7e7b21a0a26107
nahlaFathy <nehal.fathy.fci.scu@gmail.com>
2021-03-13 22:49:08 +0200

    ignore node modules

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if(!process.env.SECRET_KEY)
   process.exit(1)
 }
 
-mongoose.connect('mongodb://localhost/nodedb',{useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect(process.env.MONGO_DB||'mongodb://localhost/nodedb',{useNewUrlParser: true , useUnifiedTopology: true})
 .then(()=> console.log('connected to MongodDB ...'))
 .catch((err)=>console.error('can not connect to MongoDB',err))
 
