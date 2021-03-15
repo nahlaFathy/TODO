780af5f0ed3e20f124d131054f7f1372192d81b0
nahlaFathy <nehal.fathy.fci.scu@gmail.com>
2021-03-15 13:55:07 +0200

    last edit

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if(!process.env.SECRET_KEY)
   process.exit(1)
 }
 
-mongoose.connect(process.env.MONGO_LOCAL||process.env.MONGO_DB,{useNewUrlParser: true , useUnifiedTopology: true})
+mongoose.connect("mongodb+srv://todo:todo@cluster0.wrga0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{useNewUrlParser: true , useUnifiedTopology: true})
 .then(()=> console.log('connected to MongodDB ...'))
 .catch((err)=>console.error('can not connect to MongoDB',err))
 
